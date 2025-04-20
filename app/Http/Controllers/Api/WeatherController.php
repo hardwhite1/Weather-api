0a284676f97976fd2d23c4b2a556298b1270c28e
jackson-muema <muthianjackson1@gmail.com>
2025-04-20 21:10:05 +0300

    Creation of WeatherController

--- app/Http/Controllers/Api/WeatherController.php
+++ app/Http/Controllers/Api/WeatherController.php
@@ -7,5 +7,19 @@ use Illuminate\Http\Request;
 
 class WeatherController extends Controller
 {
-    //
+    
+
+    public function getWeather(Request $request)
+    {
+        $city = $request->query('city', 'Nairobi');
+        $apiKey = env('OPENWEATHER_API_KEY');
+
+        $response = Http::get("https://api.openweathermap.org/data/2.5/weather", [
+            'q' => $city,
+            'appid' => $apiKey,
+            'units' => 'metric'
+        ]);
+
+        return response()->json($response->json());
+    }
 }
